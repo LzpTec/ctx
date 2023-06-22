18ec9f76babb2a172cd89ea9d90d2a4294fa758a
André Posso <admin@lzptec.com>
2023-06-22 14:04:00 -0300

    feat: Implemented `AsyncContext.has`

--- src/context/async-context.ts
+++ src/context/async-context.ts
@@ -29,11 +29,13 @@ export class AsyncContext {
     }
 
     /**
-     * 
+     * Store a value in the context. If a value is already present it will be overwritten.
+     *
      * @template T
-     * @param {ContextToken<T>} token 
-     * @param {T} value
-     * @returns {AsyncContext | undefined}
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     * @param {T} value The value to store.
+     *
+     * @returns {Context} this.
      */
     public set<T>(token: ContextToken<T>, value: T): AsyncContext {
         this.#getStore().set(token, value);
@@ -41,19 +43,35 @@ export class AsyncContext {
     }
 
     /**
-     * 
+     * Retrieve the value associated with the given token.
+     *
      * @template T
-     * @param {ContextToken<T>} token 
-     * @returns {T | undefined}
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {T} The stored value or default.
      */
     public get<T>(token: ContextToken<T>): T | undefined {
         return this.#getStore().get(token);
     }
 
     /**
-     * 
-     * @param {ContextToken<unknown>} token 
-     * @returns {AsyncContext | undefined}
+     * Check if there is a value associated with the provided token.
+     *
+     * @template T
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {boolean}
+     */
+    public has<T>(token: ContextToken<T>): boolean {
+        return this.#getStore().has(token);
+    }
+
+    /**
+     * Delete the value associated with the given token.
+     *
+     * @param {ContextToken<unknown>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {Context} this.
      */
     public delete(token: ContextToken<unknown>): AsyncContext {
         this.#getStore().delete(token);
@@ -61,7 +79,7 @@ export class AsyncContext {
     }
 
     /**
-     * @returns {Array<ContextToken<unknown>> | undefined} a list of tokens currently stored in the context.
+     * @returns {Array<ContextToken<unknown>>} a list of tokens currently stored in the context.
      */
     public keys(): Array<ContextToken<unknown>> {
         return this.#getStore().keys();
