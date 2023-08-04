835552aa32baa4b81dd42ac56569e5e106a5eea3
André Posso <admin@lzptec.com>
2023-08-04 10:19:21 -0300

    feat: Added safe methods.

--- src/context/async-context.ts
+++ src/context/async-context.ts
@@ -20,12 +20,33 @@ export class AsyncContext {
         return this.#storage.run(new Context(), callback);
     }
 
-    #getStore() {
+    #getStorageSafe() {
         const storage = this.#storage.getStore();
+        return storage;
+    }
+
+    #getStorage() {
+        const storage = this.#getStorageSafe();
+
         if (!storage)
             throw new ReferenceError('Cannot access any value outside of context');
 
         return storage;
+    }
+
+    /**
+     * Store a value in the context. If a value is already present it will be overwritten.
+     * Safe Version.
+     *
+     * @template T
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     * @param {T} value The value to store.
+     *
+     * @returns {Context} this.
+     */
+    public setSafe<T>(token: ContextToken<T>, value: T): AsyncContext {
+        this.#getStorageSafe()?.set(token, value);
+        return this;
     }
 
     /**
@@ -38,8 +59,21 @@ export class AsyncContext {
      * @returns {Context} this.
      */
     public set<T>(token: ContextToken<T>, value: T): AsyncContext {
-        this.#getStore().set(token, value);
+        this.#getStorage().set(token, value);
         return this;
+    }
+
+    /**
+     * Retrieve the value associated with the given token.
+     * Safe Version.
+     *
+     * @template T
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {T} The stored value, default or undefined.
+     */
+    public getSafe<T>(token: ContextToken<T>): T | undefined {
+        return this.#getStorageSafe()?.get(token);
     }
 
     /**
@@ -50,8 +84,21 @@ export class AsyncContext {
      *
      * @returns {T} The stored value or default.
      */
-    public get<T>(token: ContextToken<T>): T | undefined {
-        return this.#getStore().get(token);
+    public get<T>(token: ContextToken<T>): T {
+        return this.#getStorage().get(token);
+    }
+
+    /**
+     * Check if there is a value associated with the provided token.
+     * Safe Version.
+     *
+     * @template T
+     * @param {ContextToken<T>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {boolean}
+     */
+    public hasSafe<T>(token: ContextToken<T>): boolean {
+        return this.#getStorageSafe()?.has(token) ?? false;
     }
 
     /**
@@ -63,7 +110,20 @@ export class AsyncContext {
      * @returns {boolean}
      */
     public has<T>(token: ContextToken<T>): boolean {
-        return this.#getStore().has(token);
+        return this.#getStorage().has(token);
+    }
+
+    /**
+     * Delete the value associated with the given token.
+     * Safe Version.
+     *
+     * @param {ContextToken<unknown>} token The reference to an instance of `ContextToken`.
+     *
+     * @returns {Context} this.
+     */
+    public deleteSafe(token: ContextToken<unknown>): AsyncContext {
+        this.#getStorageSafe()?.delete(token);
+        return this;
     }
 
     /**
@@ -74,15 +134,24 @@ export class AsyncContext {
      * @returns {Context} this.
      */
     public delete(token: ContextToken<unknown>): AsyncContext {
-        this.#getStore().delete(token);
+        this.#getStorage().delete(token);
         return this;
+    }
+
+    /**
+     * Safe Version.
+     * 
+     * @returns {Array<ContextToken<unknown>>} a list of tokens currently stored in the context.
+     */
+    public keysSafe(): Array<ContextToken<unknown>> {
+        return this.#getStorageSafe()?.keys() ?? [];
     }
 
     /**
      * @returns {Array<ContextToken<unknown>>} a list of tokens currently stored in the context.
      */
     public keys(): Array<ContextToken<unknown>> {
-        return this.#getStore().keys();
+        return this.#getStorage().keys();
     }
 
 }
